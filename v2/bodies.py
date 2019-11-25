1b2714b816c82a44e0868a266e503dbcf5cc0dbd
Menno <devemond@gmail.com>
2019-11-25 16:02:58 +0100

    Fixed sim, no colisions

--- v2/bodies.py
+++ v2/bodies.py
@@ -14,7 +14,7 @@ class Bodies:
         # Body initialization
         self.num = pos.shape[0]
         self.pos = pos
-        self.vel = velhhh`
+        self.vel = vel
         self.dt = DT
         self.mass = mass * np.ones((self.num, 1))
         self.radius = np.cbrt(3/(np.pi*4)*self.mass/RHO)
@@ -29,21 +29,21 @@ class Bodies:
         remlist = []
     
         for i in range(0, self.num):
-            if i not in remlist:
-                dist = np.add(self.pos, - self.pos[i][:])
-                nrmdist = np.linalg.norm(cp.copy(dist), axis=1)
-                nrmdist[i] = 999E10
-                test = np.reshape(self.radius + self.radius[i], (self.num))
-                check = np.where(nrmdist <= test)
-                if check[0].size:
-                    for n in check[0]:
-                        self.collide(i, n)
-                        acc = cp.deepcopy(np.delete(acc, n, axis=0))
-                        remlist.append(n)
-                else:
-                    div = np.reshape(np.power(nrmdist,3), (self.num,1))
-                    accmat = np.multiply( np.divide(dist, div), self.mass)
-                    acc[i][:] =  G * np.sum(accmat, axis=0)
+            # if i not in remlist:
+            dist = np.add(self.pos, - self.pos[i][:])
+            nrmdist = np.linalg.norm(cp.copy(dist), axis=1)
+            nrmdist[i] = 999E10
+                # test = np.reshape(self.radius + self.radius[i], (self.num))
+                # check = np.where(nrmdist <= test)
+                # if check[0].size:
+                #     for n in check[0]:
+                #         self.collide(i, n)
+                #         # acc = cp.deepcopy(np.delete(acc, n, axis=0))
+                #         # remlist.append(n)
+                # else:
+            div = np.reshape(np.power(nrmdist,3), (self.num,1))
+            accmat = np.multiply( np.divide(dist, div), np.reshape(self.mass, (self.num,1)))
+            acc[i][:] =  G * np.sum(accmat, axis=0)
         return acc
 
 
@@ -63,12 +63,12 @@ class Bodies:
     
     def collide(self, i, j):
         '''Perform an inelastic collision'''
-        self.vel[i][:]  = (self.mass[i] * self.vel[i][:] + self.mass[j] * self.vel[j][:]) / (self.mass[i] + self.mass[j])
-        self.mass[i]    = self.mass[i] + self.mass[j]
-        self.pos[i][:]  = (self.pos[i][:] + self.pos[j][:]) / 2
-        self.updateradius(i)
-        self.mass       = (np.delete(self.mass, j))
-        self.radius     = (np.delete(self.radius, j))
-        self.pos        = (np.delete(self.pos, j, axis=0))
-        self.vel        = (np.delete(self.vel, j, axis=0))
-        self.num -= 1
+        # self.vel[i][:]  = (self.mass[i] * self.vel[i][:] + self.mass[j] * self.vel[j][:]) / (self.mass[i] + self.mass[j])
+        # self.mass[i]    = self.mass[i] + self.mass[j]
+        # self.pos[i][:]  = (self.pos[i][:] + self.pos[j][:]) / 2
+        # self.updateradius(i)
+        # self.mass       = (np.delete(self.mass, j))
+        # self.radius     = (np.delete(self.radius, j))
+        # self.pos        = (np.delete(self.pos, j, axis=0))
+        # self.vel        = (np.delete(self.vel, j, axis=0))
+        # self.num -= 1
